e31b8d050e5c1872381fe5cac53f16f505e0d1ba
Felopater Melika <felopatermelika@gmail.com>
2023-06-21 13:59:53 -0500

    updates

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,5 +5,9 @@ import Providers from '@/components/Providers';
 import CartTable from '../../components/CartList';
 
 export default function Page() {
-  return <CartTable />;
+  return (
+    <Providers>
+      <CartTable />
+    </Providers>
+  );
 }
